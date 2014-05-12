5e3908f4036a4c8854173d9696d0ed7e341a08e5
Flo Mincucci <flomincucci@gmail.com>
2014-05-12 17:01:02 -0300

    rewriting everything to fit my needs

---
+++ js/keyboard.js
@@ -0,0 +1,112 @@
+(function($){
+    $.fn.extend({
+
+        keyboard: function() {
+            console.log("keyboard");
+        },
+
+        build_keyboard: function(){
+            var container = this;
+            var destiny = jQuery("input.text:first");
+            var layouts = {
+                "qwerty": [
+                    ['1 2 3 4 5 6 7 8 9 0'],
+                    ['Q W E R T Y U I O P'],
+                    ['A S D F G H J K L Ñ'],
+                    ['Z X C V B N M {bksp}' ],
+                    ['{space} {next}' ],
+                ],
+                "num": [
+                    ['1 2 3'],
+                    ['4 5 6'],
+                    ['7 8 9'],
+                    ['0 {bksp} {accept}']
+                ]
+            };
+            var keyBtn = jQuery('<input />')
+                    .attr('type','button')
+                    .addClass('ui-keyboard-button')
+                    .addClass('ui-state-default');
+            var actionKey = keyBtn.clone()
+                    .addClass('ui-keyboard-actionkey');
+
+            for( row in layouts['qwerty'] ){
+                currentRow = layouts['qwerty'][row];
+                newRow = jQuery('<div></div>')
+                    .attr('id','keyboard-row'+row)
+                    .addClass('keyboard-row')
+                    .appendTo(container);
+
+                for( set in currentRow ){
+                    newSet = jQuery('<div></div>')
+                        .addClass('ui-keyboard-keyset')
+                        .appendTo(newRow);
+                    if(set==1){
+                        //Support for modifiers
+                        newSet
+                            .addClass('ui-keyboard-shiftset')
+                            .hide();
+                    }
+                    currentSet = currentRow[set];
+                    keys = currentSet.split(/\s+/);
+                    for( key in keys ){
+                        //if it's an action key
+                        if( /^{\S+}$/.test(keys[key])){
+                            action = keys[key].match(/^{(\S+)}$/)[1];
+                            switch(action){
+                                case "space":
+                                    actionKey.clone()
+                                        .attr('name','key_space')
+                                        .val('Space')
+                                        .addClass('ui-keyboard-space')
+                                        .click(function(){
+                                            destiny.val( 
+                                                destiny.val() + ' ');
+                                        })
+                                    .appendTo(newSet);
+                                    break;
+                                case "bksp":
+                                    actionKey.clone()
+                                        .attr('name','key_bksp')
+                                        .val('Borrar')
+                                        .addClass('ui-keyboard-bksp')
+                                        .click(function(){
+                                            destiny.val(
+                                                destiny.val().substring(0,destiny.val().length - 1));
+                                        })
+                                    .appendTo(newSet);
+                                    break;
+                                case "accept":
+                                    actionKey.clone()
+                                        .attr('name','key_accept')
+                                        .val('Aceptar')
+                                        .addClass('ui-keyboard-accept')
+                                    .appendTo(newSet);
+                                    break;
+                                case "next":
+                                    actionKey.clone()
+                                        .attr('name','key_next')
+                                        .val('Siguiente')
+                                        .addClass('ui-keyboard-next')
+                                        .click(function(){
+                                            destiny = destiny.next("input.text")
+                                        })
+                                    .appendTo(newSet);
+                                    break;
+                            }
+                        } else {
+                            keyBtn.clone()
+                                .attr('name','key_'+row+'_'+key)
+                                .val(keys[key])
+                                .click(function(){
+                                    destiny.val(destiny.val() + this.value)
+                                })
+                                .appendTo(newSet);
+                        }
+                    }
+                }
+            }
+        }
+
+    });
+})(jQuery);
